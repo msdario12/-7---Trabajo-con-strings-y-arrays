6ad3f0a8f7cd3e3e9b9ed2037eef2558ae7520b7
Dario Ignacio Mansilla <mansilladario94@gmail.com>
2023-04-17 18:49:28 -0300

    Manejo de 3 frases

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,57 @@
 // Todas las frases deben empezar por mayuscula y el resto esta en minusculas. Excepto si es una frase compuesta, debe estar toda en mayusculas.
 // Mostrar el resultado de los 3 arrays por consola y ademas indicar cuando se filtra la frase en que array se guardara y porque.
 // Adjuntar link al repo.
+
+// const frase = prompt('Por favor ingrese una frase...')
+const frase = "YENDO AL PARQUE. ASF";
+
+const frase1 = prompt("Ingrese la frase N°1");
+const frase2 = prompt("Ingrese la frase N°2");
+const frase3 = prompt("Ingrese la frase N°3");
+
+const arrayFrases = [frase1, frase2, frase3];
+
+let fraseCorta = [];
+let fraseLarga = [];
+let fraseCompuesta = [];
+
+for (let frase of arrayFrases) {
+	const arrayOraciones = frase.split(". ");
+
+	// Checkeo si es una frase compuesta
+	if (arrayOraciones.length > 1) {
+		console.log(`La frase ingresada "${frase}" ingresada es una compuesta`);
+		// Verifico que todos los caracteres esten en mayusculas
+		if (frase.toUpperCase() !== frase) {
+			console.log(
+				"Todos los caracteres de una frase compuesta deben ser en mayusculas"
+			);
+		}
+		fraseCompuesta.push(frase);
+	} else {
+		// Control de capitalizacion de la primera letra
+		if (frase[0].toUpperCase() !== frase[0]) {
+			console.log(`La primera letra de ${frase} debe estar en mayusculas.`);
+		}
+		// Control del resto de caracteres en minusculas
+		for (let i = 1; i < frase.length; i++) {
+			if (frase[i].toLowerCase() !== frase[i]) {
+				console.log(
+					`El resto de letras de la frase "${frase}" deben estar en minusculas`
+				);
+				break;
+			}
+		}
+
+		if (frase.length <= 20) {
+			console.log(`La frase ingresada: "${frase}" es una frase corta`);
+			fraseCorta.push(frase);
+		} else {
+			console.log(`La frase ingresada: "${frase}" es una frase larga`);
+			fraseLarga.push(frase);
+		}
+	}
+}
+console.log(`El array de frases cortas es: [ ${fraseCorta} ].`);
+console.log(`El de frases largas: [ ${fraseLarga} ].`);
+console.log(`El de frase compuestas es:  [ ${fraseCompuesta} ]`);
